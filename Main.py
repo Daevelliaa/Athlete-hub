91e35459a0037fa522f21f5a2ad287bcaf47a145
Daevelliaa <plancon.ugo@gmail.com>
2025-03-20 16:05:44 +0100

    Annotations

--- Main.py
+++ Main.py
@@ -1,1 +1,12 @@
-print("hello")
+from dash import Dash, html
+
+app = Dash(__name__)
+
+app.layout = html.Div(children=[
+    html.H1('Bonjour, monde !'),
+    html.P('Bienvenue dans votre première application Dash.')
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+

5ffd39e77599c9a3624d1cee6eade7732a5f568c
Daevelliaa <plancon.ugo@gmail.com>
2025-03-21 13:27:16 +0100

    changement URL

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request
 
 app=Flask(__name__)
 
@@ -8,8 +8,20 @@ def home():
 
 @app.route("/connect")
 def connect():
-    strava_oauth_url="http://www.strava.com/oauth/authorize?client_id=149550&response_type=code&redirect_uri=http://localhost/exchange_token&approval_prompt=force&scope=read"
+    strava_oauth_url="http://www.strava.com/oauth/authorize?client_id=149550&response_type=code&redirect_uri=http://127.0.0.1:5000/callback&approval_prompt=force&scope=read_all,activity:read_all,profile:read_all"
     return redirect(strava_oauth_url)
+
+@app.route('/callback')
+def callback():
+    error = request.args.get('error')
+    code = request.args.get('code')
+
+    if error == 'access_denied':
+        return "L'utilisateur a refusé la connexion à Strava ❌"
+    elif code:
+        return f"Connexion réussie ! Code reçu : {code}"
+    else:
+        return "Erreur inconnue"
 
 
 if __name__=="__main__":

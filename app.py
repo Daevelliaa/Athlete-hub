526c518abae40268813b50432b11fd8962c6f6df
Daevelliaa <plancon.ugo@gmail.com>
2025-03-21 12:46:25 +0100

    Ajout route connect vers Oauth Strava

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app=Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/connect")
+def connect():
+    strava_oauth_url="http://www.strava.com/oauth/authorize?client_id=149550&response_type=code&redirect_uri=http://localhost/exchange_token&approval_prompt=force&scope=read"
+    return redirect(strava_oauth_url)
 
 
 if __name__=="__main__":
